4ea0be9d634c782b3c6ad6d1d4b6e5b487e04f73
root <root@kali>
2022-07-16 23:12:24 +0530

    counting numbers and reverse

--- basicJava/src/basicJava/CountDigitsInNum.java
+++ basicJava/src/basicJava/CountDigitsInNum.java
@@ -9,11 +9,13 @@ public class CountDigitsInNum {
 		
 		long num = sc.nextLong();
 		long holdtheNum=num;
+		long reverseNumber=0;
 		//we have to count the occurrence of digit in the number
 		int digitToFind = sc.nextInt();
 		int count=0;
-		while(num/10 > 0) {
-			int digit = (int) (num % 10);
+		while(num > 0) {
+			long digit = (num % 10);
+			reverseNumber = reverseNumber*10+digit;
 			if(digit==digitToFind)
 			{
 				count++;
@@ -21,6 +23,7 @@ public class CountDigitsInNum {
 			num=num/10;
 		}
 		System.out.println("we have "+digitToFind+ " "+count+" times in " +holdtheNum);
+		System.out.println("The Reverse of given number is "+reverseNumber);
 		sc.close();
 	}
 
