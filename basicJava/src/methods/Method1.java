93842bfcc3f6d1f9be73a1cd96a1f20eb8ebec13
root <root@kali>
2022-07-17 12:06:26 +0530

    Method creation and use

---
+++ basicJava/src/methods/Method1.java
@@ -0,0 +1,45 @@
+package methods;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Method1 extends Object{
+
+	public static void main(String[] args) {
+//		int p = Math.addExact(-1, 2);  //inbuilt method
+//		System.out.println(p);
+//		
+		//ArrayList<Character> A = new ArrayList<>();
+		String A = "RRGGRRGGGGRR";  //find GGRR or RRGG how many times 
+		int count=0;
+		for(int i = 0;i<A.length()-3;i++) {
+			if(A.charAt(i)!=A.charAt(i+1)) {
+				continue;
+			}
+			ArrayList<String> temp = new ArrayList<>();
+			
+				temp.add(A.substring(i,i+2));
+				temp.add(A.substring(i+2,i+4));
+				//System.out.println(temp.get(0));
+				count=count+patternCounter(temp);
+			
+		}
+		System.out.println(count);
+	}
+	
+	static int patternCounter(ArrayList<String> temp) {
+		int count=0;
+		if(temp.get(0).equals("RR")) {
+			if(temp.get(1).equals("GG")) {
+				count++;
+			}
+		}else if(temp.get(0).equals("GG")) {
+			if(temp.get(1).equals("RR")) {
+				count++;
+			}
+		}
+		return count;
+	}
+	
+	
+}
