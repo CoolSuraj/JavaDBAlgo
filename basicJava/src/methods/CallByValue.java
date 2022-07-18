de91afb4d828c706637a48e74edb0c8e1b1d625a
root <root@kali>
2022-07-18 10:34:51 +0530

    some clarifications

--- basicJava/src/methods/CallByValue.java
+++ basicJava/src/methods/CallByValue.java
@@ -8,9 +8,19 @@ public class CallByValue {
 		int[] arr= {1,2,3,4,5,6};
 		changeFirstValue(arr);
 		System.out.println(Arrays.toString(arr));
+		int a=3,b=4;
+		swap(a,b);
 	}
 
-	 static void changeFirstValue(int[] nums) {
+	  static  void swap(int i, int j) {
+		
+		int temp =i;
+		i=j;
+		j=temp;
+		//this value will not change actual a & b as here we are not passing reference
+	}
+
+	static void changeFirstValue(int[] nums) {
 		nums[0]=99;
 		//this will actually change arr in main
 		//This happens because we pass the reference which is pointing to same array and 
