64a3bfd008bd36421d7007e2fa9040b1e32fae3c
root <root@kali>
2022-07-18 11:20:54 +0530

    variable number of args

---
+++ basicJava/src/methods/VariableArguments.java
@@ -0,0 +1,24 @@
+package methods;
+
+import java.util.Arrays;
+
+public class VariableArguments {
+
+	public static void main(String[] args) {
+			fun(2,3,4,55,6,7,8);
+			System.out.println(sumOfAnyInts(2,3,4,55,6,7,8));
+	}
+	
+	static void fun(int ...v) {
+		//variable number of args
+		System.out.println(Arrays.toString(v));
+		System.out.println(v[2]);
+	}
+	static int sumOfAnyInts(int ...nums) {
+		int sum=0;
+		for (int i : nums) {
+			sum+=i;
+		}
+	return sum;
+	}
+}
