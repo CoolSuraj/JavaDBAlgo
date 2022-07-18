a3ccc39c131e01f831ef1e8c0aa1d267001a3a5a
root <root@kali>
2022-07-18 11:27:42 +0530

    multiple rules of var args

--- basicJava/src/methods/VariableArguments.java
+++ basicJava/src/methods/VariableArguments.java
@@ -7,6 +7,9 @@ public class VariableArguments {
 	public static void main(String[] args) {
 			fun(2,3,4,55,6,7,8);
 			System.out.println(sumOfAnyInts(2,3,4,55,6,7,8));
+			int[] arr = {2,3,4,55,6,7,8};
+			System.out.println(sumOfAnyInts(arr)); //you can also pass the array
+			sumOfAnyIntsWithMsg("Suraj", arr);
 	}
 	
 	static void fun(int ...v) {
@@ -19,6 +22,15 @@ public class VariableArguments {
 		for (int i : nums) {
 			sum+=i;
 		}
+	return sum;
+	}
+	
+	static int sumOfAnyIntsWithMsg(String msg,int ...nums) {  /*//The variable argument type int of the method sumOfAnyIntsWithMsg must be the last parameter*/
+		int sum=0;
+		for (int i : nums) {
+			sum+=i;
+		}
+		System.out.println("we can have multiple different params as well right "+msg);
 	return sum;
 	}
 }
